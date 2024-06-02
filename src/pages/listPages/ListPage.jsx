660ba00149b2079f377d531339444956e4310a77
Ekere Felix <babastanloo@gmail.com>
2024-06-02 11:03:54 +0400

    map done

--- src/pages/listPages/ListPage.jsx
+++ src/pages/listPages/ListPage.jsx
@@ -2,21 +2,25 @@ import { listData } from "../../lib/dummyData";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
 import "./listPage.scss";
+import Map from "../../components/map/Map";
+
 
 const ListPage = () => {
   const data = listData;
 
   return (
-    <div className="listPage">
+    <div className="listPage"> 
       <div className="listContainer">
         <div className="wrapper">
           <Filter />
           {data.map((item) => (
-            <Card key={item.id} item={item}/>
+            <Card key={item.id} item={item} />
           ))}
         </div>
       </div>
-      <div className="mapContainer">Map</div>
+      <div className="mapContainer">
+        <Map items={data}/>
+      </div>
     </div>
   );
 };
